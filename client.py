b8814f12d8bd1742dc36d359f6823ab036ea7ab1
grobe <grobe@localhost.localdomain>
2018-01-08 20:16:45 +0200

    new file:   client.py

---
+++ client.py
@@ -0,0 +1,35 @@
+import socket
+import select
+SERVER_IP = ""
+PORT = 0
+
+
+class ChatClient(object):
+    """docstring for ClassName"""
+    def __init__(self):
+        self.sock = socket.socket()
+        self.sock.connect((SERVER_IP, int(PORT)))
+        self.run()
+
+    def run(self):
+        command = ""
+        while "/exit" not in command:
+            if "/send" in command[:5]:
+                command = command.replace("/send", "")
+                command = command.strip()
+                arg1 = str(command[:command.index(" ")])
+                arg2 = str(command[command.index(" ") + 1:])
+                self.send(arg1, arg2)
+            command = raw_input()
+
+        self.exit()
+
+    def send(self, dest, content):
+        self.sock.send(str(dest + ":" + content))
+ 
+    def exit(self):
+        self.socket.close()
+
+
+if __name__ == '__main__':
+    me = ChatClient()
