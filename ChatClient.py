aba3fe7ad604574d0b6949ef881639cc8769fa18
snapg <snapgaming5@gmail.com>
2018-01-15 19:00:47 +0200

    look at the TODO in the client

---
+++ ChatClient.py
@@ -0,0 +1,71 @@
+import socket
+import select
+import sys
+
+
+class ChatClient(object):
+    def __init__(self, name, host='127.0.0.1', port=9955):
+        self.port = int(port)
+        self.name = name
+        self.host = host
+        # Connect to server at port
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((host, self.port))
+            # Send my name...
+            while not self.check_name():
+                self.name = raw_input(self.name + " Is not available.\n Enter your name: ")
+            print 'Connected to chat server@%d as %s' % (self.port, self.name)
+            # Initial prompt
+            self.prompt = '[' + '@'.join((name, socket.gethostname().split('.')[0])) + ']> '
+            # Contains client address, set it
+            data = self.sock.recv(1024)
+            addr = data.split('CLIENT: ')[1]
+            self.prompt = '[' + '@'.join((self.name, addr)) + ']> '
+        except socket.error:
+            print 'Could not connect to chat server @%d' % self.port
+            sys.exit(1)
+
+    def run(self):
+        command = ""
+        sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        inputs = [self.sock, sock2]
+        # TODO program has started getting stuck on this line for no reason (line 34)
+        ready_to_read, ready_to_write, in_error = select.select(inputs, [], [])
+        while "/exit" not in command:
+            for i in ready_to_read:
+                if i != self.sock:
+                    print 'write what you want\n write /help for help'
+                    command = raw_input()
+                    if "/help" in command[:5]:
+                        print "A dot indicates the end of the syntax, {x} are neccesary arguements."
+                        print "The commands available to you and their syntax:"
+                        print "/exit. Exit the chat"
+                        print "/help. View this menu"
+                        print "/send {user_dest} {data}. Send a message, {user_dest} can also be broadcast " \
+                              "(send to everyone) if it is wished."
+                        print "-----------------------"
+                    elif "/sendall in command[:8]":
+                        command = command.replace("/sendall", "")
+                        self.sock.send("broadcast" + command)
+                    elif "/send" in command[:5]:
+                        command = command.replace("/send", "")
+                        self.sock.send(command)
+                elif i == self.sock:
+                    data = self.sock.recv(1024)
+                    if data:
+                        print data + '   *'
+        self.exit()
+
+    def exit(self):
+        self.sock.send("bye")
+        self.sock.close()
+
+    def check_name(self):
+        self.sock.send("/name " + self.name)
+        return self.sock.recv(1024) == 'ok'
+
+
+if __name__ == '__main__':
+    me = ChatClient("John")
+    me.run()
--- ChatServer.py
+++ ChatServer.py
@@ -2,14 +2,21 @@ import socket
 import select
 
 
-class ChatServer:
-    def __init__(self, port=9955, ip=''):
+class ChatServer(object):
+    def __init__(self, port=9955, host='127.0.0.1'):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind(ip, port)
+        self.server.bind((host, port))
         self.online = []
-        self.names = []
+        self.names = {}
         print "Server is up, now listening..."
         self.server.listen(10)
+
+    def getname(self, client):
+        # Return the printable name of the
+        # client, given its socket...
+        info = self.names[client]
+        host, name = info[0][0], info[1]
+        return '@'.join((name, host))
 
     def run(self):
         # add server socket object to the list of readable connections
@@ -19,30 +26,53 @@ class ChatServer:
             # get the list sockets which are ready to be read through select
             # 4th arg, time_out  = 0 : poll and never block
             ready_to_read, ready_to_write, in_error = select.select(self.online, [], [], 0)
-            for sock in ready_to_read:
+            for s in ready_to_read:
                 # new connection
-                if sock == self.server:
-                    sockfd, addr = self.server.accept()
-                    self.online.append(sockfd)
-                    # TODO
-                    self.names.append(self.server.recv(2048))
-                    print "Client (%s, %s) connected" % addr
-                    self.broadcast("Client (%s, %s) connected" % addr)
-                # a message
-                # TODO
-                else:
-                    try:
-                        data = self.server.recv(2048)
-                        data = data.replace("/send", "")
-                        data = data.strip()
-                        arg1 = str(data[:data.index(" ")])
-                        arg2 = str(data[data.index(" ") + 1:])
-                        self.send(arg1, arg2)
-                    except:
-                        self.server.send("sorry, " + " is not connected")
+                if s == self.server:
+                    # handle the server socket
+                    client, address = self.server.accept()
+                    print 'chatserver: got connection %d from %s' % (client.fileno(), address)
+                    # Read the login name
+                    cname = client.recv(1024).split('/name ')[1]
+                    while cname in self.names.values():
+                        client.send("this name is taken")
+                        cname = client.recv(1024).split('/name ')[1]
+                    client.send("ok")
+                    print cname
+                    # Compute client name and send back
+                    client.send('CLIENT: ' + str(address[0]))
+                    self.online.append(client)
 
-    # TODO
+                    self.names[client] = cname
+                    # Send joining information to other clients
+                    msg = '\n(Connected: New client %s)' % (self.getname(client))
+                    print "Client (%s, %s) connected" % address
+                    self.broadcast("Client (%s) connected" % msg)
+                # a message
+                else:
+                    data = str(self.server.recv(1024)).split()
+                    print data
+                    if data[0] == 'broadcast':
+                        data.remove(data[0])
+                        self.broadcast(' '.join(data))
+                    elif data[0] in self.names.values():
+                        for cl in self.names.keys():
+                            if self.names[cl] == data[0] and cl in self.online:
+                                data.remove(data[0])
+                                cl.send(' '.join(data))
+                                break
+                        cl.send(self.getname(cl) + ' is offline')
+                    elif data[0] == 'bye':
+                        self.online.remove(client)
+                        self.names[client] = None
+                        client.close()
+                        if not self.online:
+                            self.server.close()
+
     def broadcast(self, msg):
         ready_to_read, ready_to_write, in_error = select.select(self.online, [], [], 0)
         for sock in ready_to_read:
             sock.send(msg)
+
+if __name__ == "__main__":
+    ChatServer().run()
