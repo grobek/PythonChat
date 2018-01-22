328fb00152dae4e69bdddba0ed55684d55479295
grobe <grobe@localhost.localdomain>
2018-01-22 13:26:21 +0200

    SUCKDICK

---
+++ chat.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+import sys
+class Server:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
+
+    def __init__(self):
+        self.sock.bind(("127.0.0.1", 9559))
+        self.sock.listen(1)
+
+    def handler(self, c, a):
+        while True:
+            data = c.recv(1024)
+            for connection in self.connections:
+                connection.send(data)
+            if not data:
+                print(str(a[0]) + ":" + str(a[1]), "disconnected")
+                self.connections.remove(c)
+                c.close()
+                break
+    def run(self):
+        while True:
+            c, a = self.sock.accept()
+            cThread = threading.Thread(target=self.handler, args=(c, a))
+            cThread.daemon = True
+            cThread.start()
+            self.connections.append(c)
+            print(str(a[0]) + ":" + str(a[1]), "connected")
+
+
+class Client:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, address):
+        self.sock.connect((address, 9559))
+        iThread = threading.Thread(target=self.sendMsg)
+        iThread.daemon = True
+        iThread.start()
+        while True:
+            data = self.sock.recv(1024)
+            if not data:
+                break
+            print (data)
+
+
+    def sendMsg(self):
+        while True:
+            self.sock.send(bytes(raw_input("")))
+
+if (len(sys.argv) > 1):
+    client = Client(sys.argv[1])
+else:
+    server = Server()
+    server.run()
