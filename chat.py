35fe044791fdafee787619436a0f5f9ed6d7d194
grobe <grobe@localhost.localdomain>
2018-01-24 21:25:44 +0200

    DONEEEEEEEEEEEEEEE

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ class Server:
     names = {}
 
     def __init__(self):
-        self.sock.bind(("10.100.102.2", 9559))
+        self.sock.bind(("127.0.0.1", 9559))
         self.sock.listen(1)
 
     def to_someone(self, data, connections, sock_source):
@@ -23,14 +23,26 @@ class Server:
                             sock.send("PM FROM @" + self.get_name(sock_source) + ": " + data)
                     print whoto, "exists"
                 else:
+                    sock_source.send("@" + whoto + " is not online")
                     print "tried sending to", whoto, "he doesn't exist."
 
+    def close_client(self, sock_source):
+        for connection in self.connections:  # what to do with commands
+            if connection != sock_source:
+                connection.send("User " + self.get_name(sock_source) + " has disconnected")
+        print(str(self.get_name(sock_source)), "disconnected")
+        del self.names[sock_source]
+        self.connections.remove(sock_source)
+        sock_source.close()
 
     def handler(self, c, a):
         while True:
             data = str(c.recv(1024))
             if data:
-                if data == '@?':
+                if data == '@#CLOSEME#':
+                    self.close_client(c)
+                    break
+                elif data == '@?':
                     c.send("ONLINE: " + str(self.names.values()))
                 elif data[0] == '@':
                     self.to_someone(data, self.connections, c)
@@ -40,13 +52,7 @@ class Server:
                         if connection != c:
                             connection.send("BROADCAST FROM @" + self.get_name(c) + ": " + data)
             if not data:
-                for connection in self.connections:  # what to do with commands
-                    if connection != c:
-                        connection.send("User " + self.get_name(c) + " has disconnected")
-                print(str(self.get_name(c)) , "disconnected")
-                del self.names[c]
-                self.connections.remove(c)
-                c.close()
+                self.close_client(c)
                 break
 
 
@@ -57,7 +63,6 @@ class Server:
         while True:
             c, a = self.sock.accept()
             name = c.recv(1024)
-
             if name in self.names.values():
                 c.send("sorry, taken.")
                 c.close()
@@ -70,25 +75,30 @@ class Server:
             self.connections.append(c)
             for connection in self.connections:  # what to do with commands
                 if connection != c:
-                    connection.send("User " + self.get_name(c), "has connected")
-            print(str(self.get_name(c)), "disconnected")
+                    connection.send("User " + self.get_name(c) + "has connected")
+            print(str(self.get_name(c)), "connected")
 
 
 class Client:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    keep_client = True
+    
     def __init__(self, address):
         self.sock.connect((address, 9559))
         self.sock.send(raw_input("Enter your name: "))
         print self.sock.recv(1024)
         iThread = threading.Thread(target=self.sendMsg)
-        iThread.daemon = True
+        iThread.daemon = False
         iThread.start()
-        while True:
-            data = self.sock.recv(1024)
+        while self.keep_client:
+            try:
+                data = self.sock.recv(1024)
+            except:
+                print "Goodbye!"
             if not data:
                 break
             print (data)
+        sys.exit(0)
 
     def help(self):
         print "USES:"
@@ -99,15 +109,15 @@ class Client:
         print "!! to exit"
 
     def sendMsg(self):
-        self.help()
-        keep = True
-        while True:  # get commands
+        self.help() 
+        while self.keep_client:  # get commands 
             commands = raw_input("")
             if commands == '??':
                 self.help()
             elif commands == '!!':
-                self.sock.close()
-                sys.exit(0)
+                self.sock.send("@#CLOSEME#")
+                self.keep_client = False
+                return None
             else:
                 self.sock.send(commands)
 
